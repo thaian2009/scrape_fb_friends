294776f2008657d33c4870d6cf3ccb548d8af9d2
msS <it.thaian2009@gmail.com>
2019-02-13 01:27:40 +0700

    conv json

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 ﻿var user_id= document.cookie.match(document.cookie.match(/c_user=(\d+)/)[1])[0];
 var fb_dtsg= document.getElementsByName('fb_dtsg')[0].value;
+var json= [];
 var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange=function() {
     if (this.readyState == 4 && this.status == 200) {
-      console.log(this.responseText);
+      text= this.responseText;
+	json = text.substring(text.indexOf('{'));
+	json= JSON.parse(json);
+	friends= json.payload.entries;
+	console.log(friends);
     }
   };
   xhttp.open("GET", "https://www.facebook.com/ajax/typeahead/first_degree.php?__a=1&filter[0]=user&lazy=0&viewer=" 
