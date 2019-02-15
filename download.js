1fa02db96ba68279d2ea73412e2c691260f3aee0
msS <it.thaian2009@gmail.com>
2019-02-15 15:01:47 +0700

    update download

--- download.js
+++ download.js
@@ -1,17 +1,6 @@
 function forceDownload(url, fileName){
     var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.responseType = "blob";
-    xhr.onload = function(){
-        var urlCreator = window.URL || window.webkitURL;
-        var imageUrl = urlCreator.createObjectURL(this.response);
-        var tag = document.createElement('a');
-        tag.href = imageUrl;
-        tag.download = fileName;
-        document.body.appendChild(tag);
-        tag.click();
-        document.body.removeChild(tag);
-    }
+    xhr.open("GET", 'http://fb.xmm/down.php?url='+ url + '&name=' + fileName, true);
     xhr.send();
 }
 
